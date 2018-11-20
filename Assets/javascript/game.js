141b6484e1a3bb775c853eb2a4b50c7ffaa401aa
unknown <vishalsindhu@aim.com>
2018-11-20 18:07:35 -0500

    almost completed

---
+++ Assets/javascript/game.js
@@ -0,0 +1,42 @@
+var computerChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var wins=0;
+var loses=0;
+var guesses=9;
+var guessesLeft=9;
+var UserGuess= [];
+var ComputerGuess= [];
+
+window.onload = function() {
+	var compGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+	computerGuess.push(compGuess);
+    console.log(computerGuess[0]);
+    
+    document.onkeyup = function(event) {
+        var playerGuess = event.key;
+        UserGuess.push(playerGuess);
+        console.log(computerGuess[0]);
+        if ((playerGuess === computerGuess[0]) && (guessesLeft > 0)) {
+            wins++;
+            guessesLeft = 9;
+            lettersGuessed.length = 0;
+            computerGuess.length = 0;
+            var compGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+            computerGuess.push(compGuess);
+            console.log(computerGuess[0]);
+        }
+        
+        else if ((playerGuess !== computerGuess[0]) && (guessesLeft > 0)) {
+            guessesLeft = guessesLeft-1;
+        }
+        
+        else {
+            losses++;
+            guessesLeft = 9;
+            lettersGuessed.length = 0;
+            computerGuess.length = 0;
+            var compGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+            computerGuess.push(compGuess);
+            console.log(computerGuess[0]);
+        }
+    }
+}
